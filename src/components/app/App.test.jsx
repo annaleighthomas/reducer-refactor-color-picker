c4b3c05f5027b637aef3ddb6ed0ace307dee45b3
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-16 22:40:09 -0400

    first passing test for app js

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
-  afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+    render (<App />);
+    
+    const display = screen.getByTestId('display');
+    expect(display).toHaveStyle({ backgroundColor: '#FF0000' });
+
+    const colorInput = screen.getByTestId('color-input');
+    fireEvent.change(colorInput, { target: { value: '#0000FF' } });
+    expect(display).toHaveStyle({ backgroundColor: '#0000FF' });
+
+    const undoButton = screen.getByText('undo');
+    fireEvent.click(undoButton);
+    expect(display).toHaveStyle({ backgroundColor: '#FF0000' });
+
+    const redoButton = screen.getByText('redo');
+    fireEvent.click(redoButton);
+    expect(display).toHaveStyle({ backgroundColor: '0000FF' });
+
   });
 });
