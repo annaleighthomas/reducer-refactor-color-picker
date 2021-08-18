c1475c7c61786930fcc0c86d3e80c8a7f68f883b
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-18 13:47:41 -0400

    passing refactor reducer

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,33 +1,65 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'UNDO':
+      return {
+        ...state,
+        after: [state.current, ...state.after],
+        current: state.before[state.before.length - 1],
+        before: state.before.slice(0, -1),
+      };
+    case 'RECORD':
+      return {
+        ...state,
+        before: [...state.before, state.current],
+        current: action.payload,
+      };
+    case 'REDO':
+      return {
+        ...state, 
+        before: [...state.before, state.current],
+        current: state.after[0],
+        after: state.after.slice(1),
+      };
+    default:
+      return state;
+  }
+}
 
 const useRecord = (init) => {
-  const [before, setBefore] = useState([]);
-  const [current, setCurrent] = useState(init);
-  const [after, setAfter] = useState([]);
+  const [state, dispatch] = useReducer(reducer, {
+    before: [],
+    current: init,
+    after: [],
+  });
 
   const undo = () => {
-    setAfter((after) => [current, ...after]);
-    setCurrent(before[before.length - 1]);
-    setBefore((before) => before.slice(0, -1));
+    dispatch({ type: 'UNDO' });
+    // setAfter((after) => [current, ...after]);
+    // setCurrent(before[before.length - 1]);
+    // setBefore((before) => before.slice(0, -1));
   };
 
   const redo = () => {
-    setBefore((before) => [...before, current]);
-    setCurrent(after[0]);
-    setAfter((after) => after.slice(1));
+    dispatch({ type: 'REDO' });
+    // setBefore((before) => [...before, current]);
+    // setCurrent(after[0]);
+    // setAfter((after) => after.slice(1));
   };
 
   const record = (val) => {
-    setBefore((before) => [...before, current]);
-    setCurrent(val);
+    dispatch({ type: 'RECORD', payload: val });
+    // setBefore((before) => [...before, current]);
+    // setCurrent(val);
   };
 
   return {
     undo,
     record,
     redo,
-    current,
+    current: state.current,
   };
 };
 
